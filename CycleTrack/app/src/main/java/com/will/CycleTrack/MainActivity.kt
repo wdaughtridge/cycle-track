c6580d3aad21eccf2af358df0d966f2b23a493a3
Will <wdaughtridge@gmail.com>
2021-04-11 18:49:13 -0400

    firebase login

--- CycleTrack/app/src/main/java/com/will/CycleTrack/MainActivity.kt
+++ CycleTrack/app/src/main/java/com/will/CycleTrack/MainActivity.kt
@@ -1,28 +1,77 @@
 package com.will.CycleTrack
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Looper
-import android.util.Log
+import android.view.View
 import android.widget.Button
-import androidx.core.app.ActivityCompat
-import com.google.android.gms.location.*
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
+import com.google.android.gms.tasks.Task
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var goToMap: Button
+    private lateinit var username: AppCompatEditText
+    private lateinit var password: AppCompatEditText
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        goToMap = findViewById(R.id.go_to_map)
+        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
 
-        goToMap.setOnClickListener {
-            val intent = Intent(this, MapsActivity::class.java)
-            startActivity(intent)
+        goToMap = findViewById(R.id.go_to_map)
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+
+        goToMap.setOnClickListener { v: View ->
+            val user: String = username.text.toString()
+            val pass: String = password.text.toString()
+
+            firebaseAuth
+                .signInWithEmailAndPassword(user, pass)
+                .addOnCompleteListener { task: Task<AuthResult> ->
+
+                    if (task.isSuccessful) {
+                        firebaseAnalytics.logEvent("login_success", null)
+
+                        val currentUser = firebaseAuth.currentUser!!
+                        val email = currentUser.email
+
+                        /*preferences.edit()
+                            .putString("username", inputtedUsername)
+                            .apply()*/
+
+                        Toast.makeText(this, "Logging in now..", Toast.LENGTH_LONG).show()
+
+                        val intent = Intent(this, MapsActivity::class.java)
+                        startActivity(intent)
+                    } else {
+
+                        val exception = task.exception
+                        val bundle = Bundle()
+                        if (exception is FirebaseAuthInvalidCredentialsException) {
+                            bundle.putString("error_type", "invalid_credentials")
+                            firebaseAnalytics.logEvent("login_failed", bundle)
+                            Toast.makeText(this, "Invalid credentials", Toast.LENGTH_LONG).show()
+                        } else {
+                            bundle.putString("error_type", "generic_failure")
+                            firebaseAnalytics.logEvent("login_failed", bundle)
+                            Toast.makeText(this, "Failed to login", Toast.LENGTH_LONG).show()
+                        }
+                    }
+
+                }
+
         }
     }
 }
